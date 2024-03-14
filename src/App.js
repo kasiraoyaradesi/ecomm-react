e54238bc9415bea86a180ac766d00d77d67b5f31
kasiraoyaradesi <you@example.com>
2024-03-14 16:11:13 +0530

    1

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { useContext, createContext, useState } from "react";
 import Products from "./components/product/Products.js";
-
 import Cart from "./components/cart/Cart.js";
 import Navbar from "./components/navbar/Navbar.js";
 import Login from "./components/user/Login.js";
-import { AppContext } from "./context/appContext.js";
+import { AppContextProvider } from "./context/appContext.js";
 import Logout from "./components/user/Logout.js";
 import Footer from "./components/footer/Footer.js";
 import Orders from "./components/order/Orders.js";
 
+export const UserContext = createContext(null);
 function App() {
   const PATH = process.env.REACT_APP_PATH;
-  const { flag, setFlag } = useContext(AppContext);
-
+  const [flag, setFlag] = useState(0);
+  const [user, setUser] = useState({ name: "", email: "", pass: "" });
+  const [users, setUsers] = useState([]);
+  const val = { flag, setFlag, user, setUser, users, setUsers };
   return (
     <div className="App">
       <UserContext.Provider value={val}>
@@ -26,9 +28,13 @@ function App() {
               <Navbar />
               <hr></hr>
               <Routes>
-                <Route path={`${PATH}/`} index element={<Products />} />
-                <Route path={`${PATH}/order`} element={<Orders />} />
-                <Route path={`${PATH}/cart`} element={<Cart />} />
+                <Route path={`${PATH}/cart`}  element={<Cart />} />
+             
+                <Route path={`${PATH}/order`}  element={<Orders />} /> 
+                <Route path={`${PATH}/`}  index   element={<Products />} /> 
+
+
+
               </Routes>
               <Footer />
             </Router>
